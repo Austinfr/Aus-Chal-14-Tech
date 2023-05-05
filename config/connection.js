7084aa2af7edb6b699cc6bd4614a88dee300a619
Austie <wustinah20@gmail.com>
2023-05-05 01:01:07 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-  ? sequelize = new Sequelize(process.env.JAWSDB_URL)
-  : sequelize = new Sequelize(
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
     process.env.NAME,
     process.env.USER,
     process.env.PASSWORD,
